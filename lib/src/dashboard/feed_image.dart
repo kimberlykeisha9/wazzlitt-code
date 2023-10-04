3ea02ebd313d32adb05cae2326e8f566ac32bc19
Kim <kimberlykeisha9@gmail.com>
2023-10-04 21:18:10 +0300

    Added staggered grid view

--- lib/src/dashboard/feed_image.dart
+++ lib/src/dashboard/feed_image.dart
@@ -111,138 +111,142 @@ class _FeedImageState extends State<FeedImage>
           if (snapshot.hasData) {
             Map<String, dynamic> imageData =
                 snapshot.data!.data() as Map<String, dynamic>;
-            return Wrap(
-              direction: Axis.vertical,
-              children: [
-                StreamBuilder<DocumentSnapshot>(
-                    stream: (imageData['creator_uid'] as DocumentReference)
-                        .snapshots(),
-                    builder: (context, creatorSnapshot) {
-                      if (creatorSnapshot.hasData) {
-                        Map<String, dynamic> data = creatorSnapshot.data!.data()
-                            as Map<String, dynamic>;
-                        return GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => Scaffold(
-                                  appBar: AppBar(
-                                      title: Text(data['username'] ?? 'null')),
-                                  body: FutureBuilder<Patrone>(
-                                      future: Patrone().getPatroneInformation(
-                                          widget.snapshot.get('creator_uid')),
-                                      builder: (context, snapshot) {
-                                        return ProfileScreen(
-                                            userProfile: snapshot.data!);
-                                      }),
+            return Container(
+              constraints: const BoxConstraints(maxWidth: 400),
+              alignment: Alignment.topCenter,
+              child: Wrap(
+                direction: Axis.vertical,
+                children: [
+                  StreamBuilder<DocumentSnapshot>(
+                      stream: (imageData['creator_uid'] as DocumentReference)
+                          .snapshots(),
+                      builder: (context, creatorSnapshot) {
+                        if (creatorSnapshot.hasData) {
+                          Map<String, dynamic> data = creatorSnapshot.data!.data()
+                              as Map<String, dynamic>;
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => Scaffold(
+                                    appBar: AppBar(
+                                        title: Text(data['username'] ?? 'null')),
+                                    body: FutureBuilder<Patrone>(
+                                        future: Patrone().getPatroneInformation(
+                                            widget.snapshot.get('creator_uid')),
+                                        builder: (context, snapshot) {
+                                          return ProfileScreen(
+                                              userProfile: snapshot.data!);
+                                        }),
+                                  ),
                                 ),
+                              );
+                            },
+                            child: Container(
+                              width: width(context),
+                              padding: const EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                color: Theme.of(context)
+                                    .colorScheme
+                                    .secondary
+                                    .withOpacity(0.25),
                               ),
-                            );
-                          },
-                          child: Container(
-                            width: width(context),
-                            padding: const EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                              color: Theme.of(context)
-                                  .colorScheme
-                                  .secondary
-                                  .withOpacity(0.25),
-                            ),
-                            child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  CircleAvatar(
-                                    foregroundImage: NetworkImage(data[
-                                            'profile_picture'] ??
-                                        'https://i.pinimg.com/474x/1e/23/e5/1e23e5e6441ce2c135e1e457dcf4f06f.jpg'),
-                                    radius: 20,
-                                    child: (data['profile_picture']) != null
-                                        ? null
-                                        : const Icon(
-                                            Icons.account_circle,
-                                            size: 40,
-                                          ),
-                                  ),
-                                  const SizedBox(width: 10),
-                                  Wrap(
-                                    direction: Axis.vertical,
-                                    alignment: WrapAlignment.start,
-                                    children: [
-                                      Text(data['username'] ?? 'null',
-                                          style: const TextStyle(
-                                              color: Colors.white,
-                                              fontWeight: FontWeight.bold)),
-                                              Text(location ?? '',
-                          style: const TextStyle(
-                            fontSize: 12,
-                              color: Colors.white)),
-                                    ],
-                                  ),
-                                  const Spacer(),
-                                  IconButton(
-                                    icon: const Icon(Icons.more_vert,
-                                        color: Colors.white),
-                                    onPressed: () {
-                                      showPopupMenu(context);
-                                    },
-                                  ),
-                                ]),
-                          ),
-                        );
-                      } else {
-                        return const SizedBox();
-                      }
-                    }),
-                Container(
-                  constraints: BoxConstraints(maxHeight: height(context) * 0.6, minWidth: width(context)),
-                  // child: Image.network(
-                  //     imageData['image'],
-                  //     fit: BoxFit.fitWidth),
-                  child: Image.asset('assets/images/home-image.png', fit: BoxFit.fitWidth),
-                ),
-                Spacer(),
-                Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: SizedBox(
-                    height: 100,
-                    child: Column(
-                      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Flexible(
-                          child: Text(
-                              (widget.snapshot.get('caption') as String?) ?? '',
-                              style: const TextStyle(color: Colors.white)),
-                        ),
-                        Row(children: [
-                          IconButton(
-                            padding: EdgeInsets.all(0),
-                            onPressed: () => _likeImage(),
-                            icon: likeIcon(),
-                          ),
-                          Text(
-                              '${(imageData['likes'] as List<dynamic>).length ?? 0}',
-                              style: const TextStyle(color: Colors.white)),
-                          IconButton(
-                            onPressed: () {},
-                            icon: const FaIcon(FontAwesomeIcons.message,
-                                color: Colors.white),
-                          ),
-                          const Text('0',
-                              style: TextStyle(color: Colors.white)),
-                        ]),
-                        Text(
-                            DateFormat('hh:mm, EEE d MMM').format(
-                                (imageData['date_created'] as Timestamp)
-                                    .toDate()),
+                              child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    CircleAvatar(
+                                      foregroundImage: NetworkImage(data[
+                                              'profile_picture'] ??
+                                          'https://i.pinimg.com/474x/1e/23/e5/1e23e5e6441ce2c135e1e457dcf4f06f.jpg'),
+                                      radius: 20,
+                                      child: (data['profile_picture']) != null
+                                          ? null
+                                          : const Icon(
+                                              Icons.account_circle,
+                                              size: 40,
+                                            ),
+                                    ),
+                                    const SizedBox(width: 10),
+                                    Wrap(
+                                      direction: Axis.vertical,
+                                      alignment: WrapAlignment.start,
+                                      children: [
+                                        Text(data['username'] ?? 'null',
+                                            style: const TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.bold)),
+                                                Text(location ?? '',
                             style: const TextStyle(
-                                color: Colors.white, fontSize: 12)),
-                      ],
+                              fontSize: 12,
+                                color: Colors.white)),
+                                      ],
+                                    ),
+                                    const Spacer(),
+                                    IconButton(
+                                      icon: const Icon(Icons.more_vert,
+                                          color: Colors.white),
+                                      onPressed: () {
+                                        showPopupMenu(context);
+                                      },
+                                    ),
+                                  ]),
+                            ),
+                          );
+                        } else {
+                          return const SizedBox();
+                        }
+                      }),
+                  Container(
+                    constraints: BoxConstraints(maxHeight: height(context) * 0.6, minWidth: width(context)),
+                    // child: Image.network(
+                    //     imageData['image'],
+                    //     fit: BoxFit.fitWidth),
+                    child: Image.asset('assets/images/home-image.png', fit: BoxFit.fitWidth),
+                  ),
+                  Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: SizedBox(
+                      height: 100,
+                      child: Column(
+                        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Flexible(
+                            child: Text(
+                                (widget.snapshot.get('caption') as String?) ?? '',
+                                style: const TextStyle(color: Colors.white)),
+                          ),
+                          Row(children: [
+                            IconButton(
+                              padding: EdgeInsets.all(0),
+                              onPressed: () => _likeImage(),
+                              icon: likeIcon(),
+                            ),
+                            Text(
+                                '${(imageData['likes'] as List<dynamic>).length ?? 0}',
+                                style: const TextStyle(color: Colors.white)),
+                            IconButton(
+                              onPressed: () {},
+                              icon: const FaIcon(FontAwesomeIcons.message,
+                                  color: Colors.white),
+                            ),
+                            const Text('0',
+                                style: TextStyle(color: Colors.white)),
+                          ]),
+                          Text(
+                              DateFormat('hh:mm, EEE d MMM').format(
+                                  (imageData['date_created'] as Timestamp)
+                                      .toDate()),
+                              style: const TextStyle(
+                                  color: Colors.white, fontSize: 12)),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             );
           } else if (snapshot.hasError) {
             return const Center(child: Text('Something went wrong'));
